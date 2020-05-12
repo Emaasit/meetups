99105f93ef33824442e803036fb89141cedab801
Daniel Emaasit <daniel.emaasit@gmail.com>
2020-05-12 11:40:22 -0400

    updated abstract

--- 2020_05_27_Deploy_PML_Streamlit/apps/app.py
+++ 2020_05_27_Deploy_PML_Streamlit/apps/app.py
@@ -3,11 +3,14 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+
+
 # Streamlit encourages well-structured code, like starting execution in a main()
 # function.
 def main():
 
     st.title("Build Probabilistic Machine Learning Apps Using Streamlit")
+
 
 if __name__ == "__main__":
     main()
